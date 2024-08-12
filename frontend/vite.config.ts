b7f46a508e37ff8eb129e234ff70b8a6c792796f
santiRendon <xantirendonc@gmail.com>
2024-08-11 20:15:09 -0500

    feat(frontend: Update Svelte preprocessors in frontend configuration files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { optimizeCss } from 'carbon-preprocess-svelte';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), optimizeCss()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
