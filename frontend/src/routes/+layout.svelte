c640e725415af7e896803ecc4d07764bc2fc4dbf
santiRendon <xantirendonc@gmail.com>
2024-08-11 20:05:30 -0500

    feat(frontend): Add carbon-components-svelte and carbon-preprocess-svelte dependencies

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import 'carbon-components-svelte/css/all.css';
+
+	// let theme = 'white'; // "white" | "g10" | "g80" | "g90" | "g100"
+	// $: document.documentElement.setAttribute('theme', theme);
+</script>
+
+<slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- App.svelte -->
+<script>
+	import { Accordion, AccordionItem } from 'carbon-components-svelte';
+</script>
+
+<Accordion>
+	<AccordionItem title="Section 1" open>Content 1</AccordionItem>
+	<AccordionItem title="Section 2">Content 2</AccordionItem>
+	<AccordionItem title="Section 3">Content 3</AccordionItem>
+</Accordion>
